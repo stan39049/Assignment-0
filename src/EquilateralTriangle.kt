c15221224d9e4c79509e0dea0495fa8f870fe49a
stan39049 <stan39049@gmail.com>
2025-01-26 21:58:47 -0500

    Added to EquilateralTriangle
    Added the a function to the class

--- src/EquilateralTriangle.kt
+++ src/EquilateralTriangle.kt
@@ -1,3 +1,5 @@
-class EquilateralTriangle {
-    
+class EquilateralTriangle(_name: String): Triangle(_name) {
+    fun setDimensions(_side: Int){
+        super.setDimensions(_side, _side, _side)
+    }
 }
