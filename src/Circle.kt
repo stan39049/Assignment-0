6dda9aec7e0ac9fa128dbcceff8fbe3b1898cf83
stan39049 <stan39049@gmail.com>
2025-01-26 21:39:31 -0500

    Added to Circle class
    Made a constructor, overrides, variables, and functions to Circle class

--- src/Circle.kt
+++ src/Circle.kt
@@ -1,2 +1,15 @@
-class Circle {
+class Circle(_name: String) : Shape(_name) {
+    private var radius: Int = 0
+
+    override fun printDimensions() {
+        println("Radius: $radius")
+    }
+
+    override fun getArea(): Double{
+        return Math.PI * radius * radius
+    }
+
+    fun setDimensions(rad: Int){
+        radius = rad
+    }
 }
